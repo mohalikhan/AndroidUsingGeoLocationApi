c423be6b0719f695b28decc0f71c55eba41bef32
Mohammad Ali <mohammad.ali@intelex.com>
2016-11-09 10:21:22 -0500

    Created project in Android Studio.

---
+++ app/src/main/java/scs2682/androidusinggeolocationapi/AppActivity.java
@@ -0,0 +1,13 @@
+package scs2682.androidusinggeolocationapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appactivity);
+    }
+}
