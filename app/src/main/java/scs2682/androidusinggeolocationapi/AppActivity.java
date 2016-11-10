e683e79e919f0ad9bdb01a3d48b258609c5c89d5
Mohammad Ali <mohammad.ali@intelex.com>
2016-11-10 15:20:15 -0500

    Implementation of initial version of Google Map API, without API key.

--- app/src/main/java/scs2682/androidusinggeolocationapi/AppActivity.java
+++ app/src/main/java/scs2682/androidusinggeolocationapi/AppActivity.java
@@ -1,13 +1,78 @@
 package scs2682.androidusinggeolocationapi;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class AppActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class AppActivity extends AppCompatActivity  implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appactivity);
+
+        final TextView addressText = (TextView) findViewById(R.id.addressText);
+        final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        final Button lookupButton = (Button) findViewById(R.id.lookup);
+
+        //Lookup click
+        lookupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Validation
+                String address = addressText.getText().toString();
+                if (TextUtils.isEmpty(address))
+                {
+                    Toast.makeText(AppActivity.this, "Address is empty", Toast.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                // first check do we have an internet connection
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context
+                        .CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if (networkInfo != null && networkInfo.isConnected()) {
+
+                    mapFragment.getMapAsync(AppActivity.this);
+                }
+                else {
+                    // no network
+                    Toast.makeText(AppActivity.this, "No network", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng toronto = new LatLng(43.6667, -79.4167);
+        mMap.addMarker(new MarkerOptions().position(toronto).title("Marker in Toronto"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(toronto));
+        mMap.moveCamera(CameraUpdateFactory.zoomBy(08.0f));
     }
 }
